d2ad40a65a4fb1471f1bfded73084fd7e2b7e263
Luis Gerardo <39955366+luis3698@users.noreply.github.com>
2023-09-15 21:12:37 -0500

    Initial commit

---
+++ administrador/js/Config.js
@@ -0,0 +1,14 @@
+// Config.js
+
+const firebaseConfig = {
+  apiKey: "xxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxx"
+};
+
+  
+  firebase.initializeApp(firebaseConfig);
+  
---
+++ bootstrap-restaurant-template/js/Config.js
@@ -0,0 +1,13 @@
+// Config.js
+
+const firebaseConfig = {
+    apiKey: "xxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxx"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
